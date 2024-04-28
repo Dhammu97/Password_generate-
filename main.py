3d611e1362160be8d9248d5a6742cd55704731d8
Dhammu97 <dhammdipmodak596@gmail.com>
2024-04-28 15:32:35 +0530

    incompleted file

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
 from tkinter import *
 
 window = Tk()
-window.title("Password Manager")
-window.config(padx=50, pady=50)
-# logo
+window.title("Password Generator")
+window.config(pady=50, padx=50, bg='yellow')
+
+
+# Logo
 canvas = Canvas(height=200, width=200)
 logo = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo)
-canvas.grid(row=0, column=0, columnspan=2)
-# label
+canvas.grid(row=0, column=1, padx=3, pady=3, columnspan=2)
 
-website_label = Label(text="Website :")
-website_label.grid(row=1, column=0)
-email_label = Label(text="Email :")
-email_label.grid(row=2, column=0)
-password_label = Label(text="Password")
-password_label.grid(row=3, column=0)
 
-# entry
+# Label
+
+website_label = Label(text='Website :')
+website_label.grid(column=0, row=1, pady=3, padx=3)
+email_label = Label(text='Email/Username :')
+email_label.grid(column=0, row=2, pady=3, padx=3)
+password_label = Label(text='Password :')
+password_label.grid(column=0, row=3, pady=3, padx=3)
+
+
+# Entry Field
 
 website_entry = Entry(width=21)
-website_entry.grid(row=1, column=1, columnspan=2)
-email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1, columnspan=2)
+website_entry.grid(row=1, column=1, pady=3, padx=3)
+email_entry = Entry(width=21)
+email_entry.grid(row=2, column=1, pady=3, padx=3)
 password_entry = Entry(width=21)
-password_entry.grid(row=3, column=1)
+password_entry.grid(row=3, column=1, pady=3, padx=3)
+
 
 # Buttons
 
-add_button = Button(text="Add", width=36)
-add_button.grid(row=4, column=1)
-search_button = Button(text="Search", width=13)
-search_button.grid(row=1, column=2)
-pass_gen = Button(text="Generate Password")
-pass_gen.grid(row=3, column=2)
+password_button = Button(text="Generate password", width=15)
+password_button.grid(column=2, row=3, columnspan=2)
+search_button = Button(text="Search", width=15)
+search_button.grid(column=2, row=1, columnspan=2)
+add_button = Button(text="ADD", width=35)
+add_button.grid(column=1, row=4, columnspan=2)
+
+
 
 window.mainloop()
